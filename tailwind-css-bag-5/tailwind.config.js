fa600759c94c712d9843bcf82a0d84a654568fb3
EvelinSA05 <evelinsalsabila27@gmail.com>
2023-08-30 10:28:16 +0700

    tailwind-css bagian 5

---
+++ tailwind-css-bag-5/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        ungu: "#8b5cf6",
+        merah: "#e11d48",
+        biru: "#3b82f6",
+        "abu-abu": "#94a3b8",
+        putih: "#f8fafc",
+      },
+
+      fontFamily: {
+        Poppins: ["Poppins, sans-serif"],
+      },
+
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          lg: "1124px",
+          xl: "1124px",
+          "2xl": "1124px",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
